bc20fafb0a6e3129036a93375830bda2ef18cbf4
Ivan <35151495+IvanKorpushenkov@users.noreply.github.com>
2018-09-04 12:20:10 +0300

    add new function SAFEtgz, some changes
    Some changes of functions bodyes. Add function for file *.SAFE.tgz (and some other archive formats like .zip, .rar etc.) open.

--- io.py
+++ io.py
@@ -1,7 +1,7 @@
 import os, os.path
 import glob
 import xml.etree.ElementTree as ET
-import gzip, zipfile
+import tarfile
 
 def SAFElink(link):
 
@@ -10,15 +10,60 @@ def SAFElink(link):
         treeSAFE = os.walk(link)
         for i in treeSAFE:
             folder_SAFE.append(i)
+    else:
+        pass
 
-    filename = folder_SAFE[2][2][0]
-    linkToFile = str(folder_SAFE[2][0])
-    xmlfile = linkToFile + '\\' + str(filename)
+    for i in range(len(folder_SAFE)):
+        for j in range(len(folder_SAFE[i])):
+            if '\\' in folder_SAFE[i][j] and 'data' \
+                    in folder_SAFE[i][j]:
+                linkToFile = folder_SAFE[i][j]
+            else:
+                pass
+            for k in range(len(folder_SAFE[i][j])):
+                if 'aux' in folder_SAFE[i][j][k] and 'xml' \
+                        in folder_SAFE[i][j][k]:
+                    filename = folder_SAFE[i][j][k]
+                else:
+                    pass
+
+    xmlfile = str(linkToFile) + '\\' + str(filename)
 
     return xmlfile
 
 def SAFEtgz(link):
 
-    gzip.decompress(link)
+    if '.TGZ' in link and '\\' not in link:
+        tar = tarfile.open(link)
+        tar.extractall()
+        tar.close()
+    else:
+        pass
 
-    return
+    folder_SAFE = []
+    files = os.listdir()
+    for i in range(len(files)):
+        if '.SAFE' in files[i]:
+            treeSAFE = os.walk(files[i])
+            for j in treeSAFE:
+                folder_SAFE.append(j)
+        else:
+            pass
+
+    for i in range(len(folder_SAFE)):
+        for j in range(len(folder_SAFE[i])):
+            if '\\' in folder_SAFE[i][j] and 'data' \
+                    in folder_SAFE[i][j]:
+                linkToFile = folder_SAFE[i][j]
+            else:
+                pass
+            for k in range(len(folder_SAFE[i][j])):
+                if 'aux' in folder_SAFE[i][j][k] and 'xml' \
+                        in folder_SAFE[i][j][k]:
+                    filename = folder_SAFE[i][j][k]
+                else:
+                    pass
+
+    xmlfile = str(linkToFile) + '\\' + str(filename)
+
+    return xmlfile
